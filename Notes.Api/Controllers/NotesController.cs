890ac8de8c5439c0b0b9e2c90afc1d5620c42114
Sebastian <sebastian@zoezi.se>
2026-08-12 17:55:47 +0200

    Fix sql injection

--- Notes.Api/Controllers/NotesController.cs
+++ Notes.Api/Controllers/NotesController.cs
@@ -34,7 +34,9 @@ public class NotesController : ControllerBase
         var user = BasicAuthenticationHandler.GetUserFrom(authorizationHeader);
 
         return _database.Notes
-            .FromSqlRaw($"SELECT * FROM Notes WHERE Author='{user.Username}' AND Content LIKE '%{containing}%' ORDER BY Id")
+            .Where(note => note.Author == user.Username)
+            .Where(note => string.IsNullOrEmpty(containing) ? true : note.Content.Contains(containing))
+            .OrderBy(note => note.Id)
             .ToArray();
     }
 
